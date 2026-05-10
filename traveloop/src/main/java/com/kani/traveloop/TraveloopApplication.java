0a260cf678b3febfa75f405b908dccdc05e45df9
kanishk424 <kanishkmaheswaran2007@gmail.com>
2026-05-10 11:07:20 +0530

    Update the session in wanderlag

---
+++ traveloop/src/main/java/com/kani/traveloop/TraveloopApplication.java
@@ -0,0 +1,13 @@
+package com.kani.traveloop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TraveloopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TraveloopApplication.class, args);
+	}
+
+}
---
+++ traveloop/src/test/java/com/kani/traveloop/TraveloopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kani.traveloop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TraveloopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
